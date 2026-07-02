88629852d3affcb0de47c6a0ad14938c1e2bcfac
Shyamsundar S S <shyamsundar93424@gmail.com>
2026-07-02 12:53:53 +0530

    added local storage

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,9 +1,11 @@
 import { defineStore } from "pinia";
+import { carts } from "./carts";
+
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
-    isLoggedIn: false,
-    currentUser: null
+    isLoggedIn: JSON.parse(localStorage.getItem("isLoggedIn")) || false,
+    currentUser: JSON.parse(localStorage.getItem("currentUser")) || null
   }),
 
   getters: {
@@ -16,16 +18,30 @@ export const useAuthStore = defineStore("auth", {
     login(user) {
       this.currentUser = user;
       this.isLoggedIn = true;
+      localStorage.setItem("currentUser", JSON.stringify(this.currentUser))
+
+      localStorage.setItem("isLoggedIn", JSON.stringify(this.isLoggedIn))
+
+
     },
     loginadmin(admin) {
       this.currentUser = admin;
       this.isLoggedIn = true;
+      localStorage.setItem("currentUser", JSON.stringify(this.currentUser))
+      localStorage.setItem("isLoggedIn", JSON.stringify(this.isLoggedIn))
+
     },
 
     logout() {
-      alert()
+      const cart = carts()
       this.currentUser = null;
       this.isLoggedIn = false;
+      //alert(`${this.currentUser} is logged out`)
+      localStorage.setItem("currentUser", JSON.stringify(this.currentUser))
+      localStorage.setItem("isLoggedIn", JSON.stringify(this.isLoggedIn))
+      cart.clearcart()
+
+
     }
   }
 });
--- src/stores/carts.js
+++ src/stores/carts.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const carts = defineStore('cart', {
   state: () => ({
-    cartItems: []
+    cartItems: JSON.parse(localStorage.getItem("cart"))||[]
   }),
   getters: {
 
@@ -10,12 +10,18 @@ export const carts = defineStore('cart', {
   actions: {
     addtocart(products) {
       this.cartItems.push(products)
+      localStorage.setItem("cart",JSON.stringify(this.cartItems))
       alert(`${products.name} added`)
     },
 
     delcart(index){
       this.cartItems.splice(index,1)
+      localStorage.setItem("cart",JSON.stringify(this.cartItems))
       alert(`product removed from the cart`)
+    },
+    clearcart(){
+      this.cartItems=[]
+      localStorage.removeItem("cart")
     }
   }
 })
